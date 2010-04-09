399f3419d24ac759ff9d1d45c4fab03a9423f1f4
John Reese <jreese@leetcode.net>
2010-04-08 20:38:55 -0400

    Properly purge the CSRF token

--- Announce/pages/create.php
+++ Announce/pages/create.php
@@ -12,6 +12,6 @@ $message = gpc_get_string("message");
 $message = new AnnounceMessage($title, $message);
 $message->save();
 
+form_security_purge("plugin_Announce_create");
 print_successful_redirect(plugin_page("list", true));
-form_security_validate("plugin_Announce_create");
 
