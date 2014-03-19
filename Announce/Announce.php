7af0fef2ac7596a18a54eafd021d1016fa6a8cd9
Bascy <bas@basbex.com>
2014-03-19 08:50:10 +0100

    a

--- Announce/Announce.php
+++ Announce/Announce.php
@@ -88,7 +88,8 @@ class AnnouncePlugin extends MantisPlugin {
 			#2010-04-14
 			array( "CreateIndexSQL", array( "idx_plugin_announce_context", plugin_table( "context" ), "message_id, project_id, location", array( "UNIQUE" ) ) ),
       			#2014-03-18
-      			array( "AddColumnSQL", array( plugin_table( "dismissed" ), "timestamp		I	NOTNULL UNSIGNED DEFAULT \" 0 \" " ) ),
+      			array( "AddColumnSQL", array( plugin_table( 'dismissed' ), 
+      			       "timestamp	I		NOTNULL UNSIGNED DEFAULT 0 " ) ),
 
 	}
 }
