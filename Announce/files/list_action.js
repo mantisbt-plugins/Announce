e639bd51dee960ffe66ec9aee4d6a4d0289726ef
Damien Regad <dregad@mantisbt.org>
2017-06-20 00:11:28 +0200

    Fix 'select all' checkbox javascript

--- Announce/files/list_action.js
+++ Announce/files/list_action.js
@@ -4,7 +4,7 @@
 jQuery(document).ready(function($) {
 		// Message list behaviors
 		$("input.announce_select_all").change(function(){
-				$("input[name='message_list[]']").attr("checked", $(this).attr("checked"));
+				$("input[name='message_list[]']").prop("checked", $(this).prop("checked"));
 			});
 
 		function delete_action (event){
