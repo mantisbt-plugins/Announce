391293602e330c146f9ff6b821bf583d15c23780
Damien Regad <dregad@mantisbt.org>
2017-07-01 18:56:15 +0200

    Bump MantisCore requirement to 2.0.0

--- Announce/Announce.php
+++ Announce/Announce.php
@@ -12,7 +12,7 @@ class AnnouncePlugin extends MantisPlugin {
 
 		$this->version = "1.0.0";
 		$this->requires		= array(
-			"MantisCore" => "1.3.0",
+			"MantisCore" => "2.0.0",
 		);
 
 		$this->author		= "John Reese";
