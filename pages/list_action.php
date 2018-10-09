bd57cdb6db6921a8ec10006eb00b1becb782a56e
Damien Regad <dregad@mantisbt.org>
2018-10-10 00:18:47 +0200

    Coding guidelines
    - whitespace
    - variable names
    - quotes

--- pages/list_action.php
+++ pages/list_action.php
@@ -4,28 +4,28 @@
 # Copyright (c) 2017 Damien Regad
 # Licensed under the MIT license
 
-access_ensure_global_level(plugin_config_get("manage_threshold"));
+access_ensure_global_level( plugin_config_get( 'manage_threshold' ) );
 
-$message_list = gpc_get_int_array("message_list", array());
+$t_message_list = gpc_get_int_array( 'message_list', array() );
 
-if (count($message_list) < 1) {
-	print_header_redirect(plugin_page("list", true));
+if( count( $t_message_list ) < 1 ) {
+	print_header_redirect( plugin_page( 'list', true ) );
 }
 
-$messages = AnnounceMessage::load_by_id($message_list, true);
-$messages = AnnounceMessage::clean( $messages, AnnounceMessage::TARGET_FORM );
+$t_messages = AnnounceMessage::load_by_id( $t_message_list, true );
+$t_messages = AnnounceMessage::clean( $t_messages, AnnounceMessage::TARGET_FORM );
 
-layout_page_header(plugin_lang_get("edit_title"));
+layout_page_header( plugin_lang_get( 'edit_title' ) );
 layout_page_begin();
 ?>
 
-<script type="text/javascript" src="<?php echo plugin_file("list_action.js") ?>"></script>
+<script type="text/javascript" src="<?php echo plugin_file( 'list_action.js' ) ?>"></script>
 
 <div class="col-md-12 col-xs-12">
 <div class="space-10"></div>
 
 <div class="form-container">
-<form action="<?php echo plugin_page("list_action_update") ?>" method="post">
+<form action="<?php echo plugin_page( 'list_action_update' ) ?>" method="post">
 
 	<?php echo form_security_field( 'plugin_Announce_list_action_update' ); ?>
 
@@ -51,104 +51,101 @@ layout_page_begin();
 					<table class="table table-striped table-bordered table-condensed">
 						<tbody>
 <?php
-	$first = true;
-	foreach($messages as $message_id => $message) {
-		$context_count = count($message->contexts);
-		if (!$first) {
+$t_first = true;
+foreach( $t_messages as $t_message_id => $t_message ) {
+	$t_context_count = count( $t_message->contexts );
+	if( !$t_first ) {
 ?>
 							<tr class="spacer"><td></td></tr>
-<?php
-		}
-?>
-
+<?php } ?>
 							<tr>
-								<td class="center announce_list_<?php echo $message_id ?>"
-									rowspan="<?php echo 3 + $context_count ?>">
+								<td class="center announce_list_<?php echo $t_message_id ?>"
+									rowspan="<?php echo 3 + $t_context_count ?>">
 									<input type="checkbox" name="message_list[]"
-										   value="<?php echo $message_id ?>"
+										   value="<?php echo $t_message_id ?>"
 										   checked="checked"/>
 								</td>
-								<td class="category" colspan="2"><?php echo plugin_lang_get("title") ?></td>
+								<td class="category" colspan="2"><?php echo plugin_lang_get( 'title' ) ?></td>
 								<td colspan="4">
-									<input name="title_<?php echo $message_id ?>"
+									<input name="title_<?php echo $t_message_id ?>"
 										   type="text" size="50"
-										   value="<?php echo $message->title ?>"
+										   value="<?php echo $t_message->title ?>"
 									/>
-									<div id="warning_last_context_<?php echo $message_id ?>"
+									<div id="warning_last_context_<?php echo $t_message_id ?>"
 										 class="alert alert-warning warning-last-context">
-										<?php echo plugin_lang_get( 'delete_last_context'); ?>
+										<?php echo plugin_lang_get( 'delete_last_context' ); ?>
 									</div>
 								</td>
 							</tr>
 
 							<tr>
-								<td class="category" colspan="2"><?php echo plugin_lang_get("message") ?></td>
+								<td class="category" colspan="2"><?php echo plugin_lang_get( 'message' ) ?></td>
 								<td colspan="4">
-									<textarea name="message_<?php echo $message_id ?>"
+									<textarea name="message_<?php echo $t_message_id ?>"
 											  cols="70" rows="4"><?php
-										echo $message->message
+										echo $t_message->message
 									?></textarea>
 								</td>
 							</tr>
 
 							<tr class="row-category">
 								<td class="category center">
-									<a class="announce_add_context" href="#" data-message-id="<?php echo $message_id ?>">
+									<a class="announce_add_context" href="#" data-message-id="<?php echo $t_message_id ?>">
 										<img src="<?php echo plugin_file("add.png") ?>" alt="+" border="0"/>
 									</a>
 								</td>
-								<td class="category"><?php echo plugin_lang_get("location") ?></td>
-								<td class="category"><?php echo plugin_lang_get("project") ?></td>
-								<td class="category"><?php echo plugin_lang_get("access") ?></td>
-								<td class="category"><?php echo plugin_lang_get("ttl") ?></td>
-								<td class="category"><?php echo plugin_lang_get("dismissable") ?></td>
+								<td class="category"><?php echo plugin_lang_get( 'location' ) ?></td>
+								<td class="category"><?php echo plugin_lang_get( 'project' ) ?></td>
+								<td class="category"><?php echo plugin_lang_get( 'access' ) ?></td>
+								<td class="category"><?php echo plugin_lang_get( 'ttl' ) ?></td>
+								<td class="category"><?php echo plugin_lang_get( 'dismissable' ) ?></td>
 							</tr>
 
 <?php
-		foreach($message->contexts as $context_id => $context) {
+	foreach( $t_message->contexts as $t_context_id => $t_context ) {
 ?>
 							<tr class="row-context">
 								<td class="center">
 									<a class="announce_delete_context" href="#"
-									   data-message-id="<?php echo $message_id ?>"
-									   data-context-id="<?php echo $context_id ?>"
+									   data-message-id="<?php echo $t_message_id ?>"
+									   data-context-id="<?php echo $t_context_id ?>"
 									>
-										<img src="<?php echo plugin_file("delete.png") ?>"
+										<img src="<?php echo plugin_file('delete.png' ) ?>"
 											 alt="-" border="0"/>
 									</a>
-									<input type="hidden" name="context_delete_<?php echo $context->id ?>" value="0"/>
+									<input type="hidden" name="context_delete_<?php echo $t_context->id ?>" value="0"/>
 								</td>
 								<td class="center">
-									<select name="location_<?php echo $context->id ?>">
-										<?php Announce::print_location_option_list($context->location) ?>
+									<select name="location_<?php echo $t_context->id ?>">
+										<?php Announce::print_location_option_list( $t_context->location ) ?>
 									</select>
 								</td>
 								<td class="center">
-									<select name="project_<?php echo $context->id ?>">
-										<?php print_project_option_list($context->project_id) ?>
+									<select name="project_<?php echo $t_context->id ?>">
+										<?php print_project_option_list( $t_context->project_id ) ?>
 									</select>
 								</td>
 								<td class="center">
-									<select name="access_<?php echo $context->id ?>">
-										<?php print_enum_string_option_list("access_levels", $context->access ) ?>
+									<select name="access_<?php echo $t_context->id ?>">
+										<?php print_enum_string_option_list( 'access_levels', $t_context->access ) ?>
 									</select>
 								</td>
 								<td class="center">
-									<input name="ttl_<?php echo $context->id ?>" class="ttl"
-										   title="<?php echo plugin_lang_get("ttl_help") ?>"
-										   type="number" value="<?php echo $context->ttl ?>" min="0"
+									<input name="ttl_<?php echo $t_context->id ?>" class="ttl"
+										   title="<?php echo plugin_lang_get( 'ttl_help' ) ?>"
+										   type="number" value="<?php echo $t_context->ttl ?>" min="0"
 									/>
 								</td>
 								<td class="center">
-									<input type="checkbox" name="dismissable_<?php echo $context->id ?>"
-										<?php check_checked((bool)$context->dismissable) ?>/>
+									<input type="checkbox" name="dismissable_<?php echo $t_context->id ?>"
+										<?php check_checked( (bool)$t_context->dismissable ) ?>/>
 								</td>
 							</tr>
 <?php
-		}
-
-		$first = false;
 	}
+
+	$t_first = false;
+}
 ?>
 						</tbody>
 
@@ -158,7 +155,7 @@ layout_page_begin();
 									<input type="checkbox" class="announce_select_all" checked="checked"/>
 								</td>
 								<td class="center" colspan="6">
-									<input type="submit" value="<?php echo plugin_lang_get("action_edit") ?>"/>
+									<input type="submit" value="<?php echo plugin_lang_get( "action_edit" ) ?>"/>
 								</td>
 							</tr>
 						</tfoot>
